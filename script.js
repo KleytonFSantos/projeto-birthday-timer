81bfe67543f4e30e83380efdd220ef6821e8c4c8
KleytonFSantos <85530567+KleytonFSantos@users.noreply.github.com>
2022-02-19 14:27:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minEl = document.getElementById('min');
+const secEl = document.getElementById('sec');
+
+
+
+
+const newYears = '17 Aug 2022';
+
+function countdown() {
+    const newYearsDate = new Date(newYears);
+    const currentDate = new Date();
+
+
+    const totalSeconds = new Date(newYearsDate - currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24)
+
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+
+    const minutes = Math.floor(totalSeconds / 60) % 60;
+
+    const seconds = Math.floor(totalSeconds % 60);
+
+
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = formatTime(hours);
+    minEl.innerHTML = formatTime(minutes);
+    secEl.innerHTML = formatTime(seconds);
+
+}
+
+function formatTime(time) {
+    return time < 10 ? (`0 ${time}`) : time;
+}
+
+// initial call
+countdown();
+
+setInterval(countdown, 1000);
